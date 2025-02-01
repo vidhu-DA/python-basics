f04b287ede4aacd377ffc1eafdaec93e2a1cddb1
vidhu-DA <vidhyasekar29@gmail.com>
2025-02-01 21:05:42 +1300

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+print (1+1)
+
+## app/py in this terminal has this enrionment set up I already have ipykernal packages
+
+## this is an example of single line comment
+'''
+this single dot multiline comments won't work in jupyter file
+this is the example of multiline comments
+'''
---
+++ assignment_1.py
@@ -0,0 +1,8 @@
+# Your code here
+a = int(input("enter the number:"))
+if a>1:
+    print("Positive")
+elif a<1:
+        print ("negative")
+        if a==0:
+            print("Zero")
